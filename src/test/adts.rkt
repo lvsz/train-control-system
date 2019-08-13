09534d652036f5d8a8bf5deed9ad5cab5ddb8a78
lvsz <levisiuzdak@gmail.com>
2019-08-13 17:22:15 +0200

    Improved detection block functionality

--- src/test/adts.rkt
+++ src/test/adts.rkt
@@ -15,13 +15,22 @@
 (define n7 (make-object node% 'n7))
 (define n8 (make-object node% 'n8))
 (define n9 (make-object node% 'n9))
-(define nodes (list n1 n2 n3 n4 n5 n6 n7 n8 n9))
+(define n10 (make-object node% 'n10))
+(define n11 (make-object node% 'n11))
+(define n12 (make-object node% 'n12))
+(define nodes (list n1 n2 n3 n4 n5 n6 n7 n8 n9 n10 n11 n12))
 
+; railway setup used for tests
+;
 ;      n1============n2
 ;       \            /
 ;  n6----}n5--n4--n3(
 ;  ||   /            \
-;  n7==n8            n9
+;  n7==n8            n9==n10==n11==n12
+;
+; double line indicates detection block
+; paren indicates regular switch (2 exits)
+; bracket indicates double switch (3 exits)
 
 (define b-n1-n2 (make-object block% 'b-n1-n2 n1 n2 500))
 (define t-n2-n3 (make-object block% 't-n2-n3 n2 n3 200))
@@ -36,6 +45,9 @@
 (define s-n5<n1-n6-n8 (make-object switch% 's-n5<n1-n6-n8 s-n5<n1-n6 t-n5-n8))
 (define t-n3-n9 (make-object track% 't-n3-n9 n3 n9 100))
 (define s-n3<n2-n9 (make-object switch% 's-n3<n2-n9 t-n2-n3 t-n3-n9))
+(define b-n9-n10 (make-object block% 'b-n9-n10 n9 n10 50))
+(define b-n10-n11 (make-object block% 'b-n10-n11 n10 n11 50))
+(define b-n11-n12 (make-object block% 'b-n11-n12 n11 n12 50))
 (define tracks (list b-n1-n2 t-n2-n3 t-n3-n4 t-n4-n5 t-n5-n1 t-n5-n6 b-n6-n7 b-n7-n8 s-n5<n1-n6 t-n5-n8 s-n5<n1-n6-n8 t-n3-n9 s-n3<n2-n9))
 
 (define node-tests
@@ -75,7 +87,7 @@
                  "Return #f when tracks don't connect"))
     (test-case
       "Given a connected track, from* should return a list of tracks on the other side"
-      (check-eq? (send t-n3-n9 from* t-n3-n4)
+      (check-eq? (send b-n11-n12 from* b-n10-n11)
                  '()
                  "Return the empty list for dead ends")
       (check-equal? (send b-n1-n2 from* t-n5-n1)
@@ -90,19 +102,28 @@
       "Detection blocks can update their status and that of any connected block"
       (check-eq? (send t-n2-n3 get-status)
                  'green)
-      (send t-n2-n3 set-status 'red)
+      (send t-n2-n3 occupy)
       (check-eq? (send t-n2-n3 get-status)
                  'red)
-      (send b-n6-n7 set-status 'red)
+      (send b-n6-n7 occupy)
       (check-eq? (send b-n6-n7 get-status)
                  'red)
       (check-eq? (send b-n7-n8 get-status)
                  'orange
                  "Block b-n7-n8 should turn orange when connected block b-n6-n7 turns red")
-      (send b-n6-n7 set-status 'green)
+      (send b-n6-n7 clear)
       (check-eq? (send b-n7-n8 get-status)
                  'green
-                 "Block b-n7-n8 should turn green when connected block b-n6-n7 turns green"))))
+                 "Block b-n7-n8 should turn green when connected block b-n6-n7 turns green")
+      (send b-n9-n10 occupy)
+      (send b-n11-n12 occupy)
+      (check-eq? (send b-n10-n11 get-status)
+                 'orange
+                 "Block b-n10-n11 has two occupied neighbours, it should be red")
+      (send b-n11-n12 clear)
+      (check-eq? (send b-n10-n11 get-status)
+                 'orange
+                 "Block b-n10-n11 should remain orange after clearing one of its neighbours"))))
 
 (define switch-tests
   (test-suite
